dfe5977f4ebbdb53d0410b2faf68ee3372379708
Alexandre Joris <alexandre.joris@govirtuo.com>
2023-02-17 18:54:42 +0100

    fixed max_token values + upload longer prompts to hastebin

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -1,23 +1,23 @@
 from cloudbot import hook
 from cloudbot.bot import bot
 import requests
+import textwrap
 
 
 @hook.command("gpt", autohelp=False)
 def chat_gpt(nick, chan, text):
     prompt = (
-        f"Answer the following message from user {nick} on IRC channel {chan}.\n"
-        f"{text}\n"
+        f"{nick} on IRC channel {chan} says: {text}\n"
     )
-    api_key = bot.config.get_api_key("openai")
+    open_ai_api_key = bot.config.get_api_key("openai")
     resp = requests.post("https://api.openai.com/v1/completions",
                            headers={
-                             "Authorization": f"Bearer {api_key}",
+                             "Authorization": f"Bearer {open_ai_api_key}",
                            },
                            json={
                                "model": "text-davinci-003",
                                "prompt": prompt,
-                               "max_tokens": 100,
+                               "max_tokens": 1024,
                                "temperature": 1,
                                "n": 1,
                                "stream": False,
@@ -25,6 +25,28 @@ def chat_gpt(nick, chan, text):
                            }
     )
     if resp.status_code == 200:
-        return resp.json()["choices"][0]["text"].replace("\n","")
+        answer = resp.json()["choices"][0]["text"].replace("\n","")
+        messages = textwrap.wrap(answer,250)
+        if len(messages) > 2:
+            # Send the prompt to hastebin
+            hastebin_api_key = bot.config.get_api_key("hastebin")
+            hastebin_resp = requests.post("https://hastebin.com/documents",
+                                          headers={
+                                              "Authorization": f"Bearer {hastebin_api_key}",
+                                              "Content-Type": "text/plain"
+                                          },
+                                          data="\n".join(messages)
+            )
+            if hastebin_resp.status_code == 200:
+                # Return first 2 blocks, then direct to hastebin
+                truncated_resp = messages[0:2]
+                truncated_resp.append(f"Find the rest of the answer here: https://hastebin.com/share/{hastebin_resp.json()['key']}") 
+                return truncated_resp
+            else:
+                return "Hastebin failed with error {hastebin_resp.status_code} and message: {hastebin_resp.json()}"
+        return messages
     else:
-         return f"ChatGPT failed with error code {resp.status_code} and message {resp.json()}"
+         return textwrap.wrap(
+             f"ChatGPT failed with error code {resp.status_code} and message: {resp.json()['error']['message']}",
+             250
+         )
