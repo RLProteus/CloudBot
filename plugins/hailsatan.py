b37034fc837723c47dbe9075d0bddc2cf9196b60
xenorim <yjones@xenorim.com>
2023-03-10 14:27:47 -0600

    changed to irccloud link for gif

--- plugins/hailsatan.py
+++ plugins/hailsatan.py
@@ -8,4 +8,4 @@ message = re.compile('666',re.IGNORECASE)
 @hook.regex(message)
 def hailsatan(match, nick, chan, db, notice):
     """no useful help txt"""
-    return "https://public1235482.blob.core.windows.net/public/hailsatan.gif"
+    return "https://usercontent.irccloud-cdn.com/file/4MTjXuQk/hailsatan.gif"
